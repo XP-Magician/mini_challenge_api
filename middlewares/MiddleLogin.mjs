364a7e194f0c92f3f19e8a46e40deaba85df0392
Diego <diegofabrega18@gmail.com>
2024-04-18 19:07:56 -0400

    UPDATE: Add token TTL of 1 hour, add middleware to check if token was sent

--- middlewares/MiddleLogin.mjs
+++ middlewares/MiddleLogin.mjs
@@ -14,9 +14,11 @@ export const validateBody = (req, res, next) => {
 export const authUser = (req, res, next) => {
   const { username, password } = req.body;
   if (username === CONFIG.USERNAME && password === CONFIG.PASSWORD) {
+    const expiresIn = "1h";
     const token = jwt.sign(
       { username, password },
-      process.env.SECRET ?? "localbackend"
+      process.env.SECRET ?? "localbackendLS(ToChange);",
+      { expiresIn }
     );
     req.x_result = {
       access_token: token,
@@ -28,5 +30,27 @@ export const authUser = (req, res, next) => {
       error: "Invalid credentials provided, check your entries",
     };
     next();
+  }
+};
+
+export const checkAuth = (req, res, next) => {
+  const authorizationHeader = req.get("authorization");
+  if (
+    !authorizationHeader ||
+    !authorizationHeader.toLowerCase().startsWith("bearer")
+  )
+    return res.status(401).json({ message: "Missing authorization header." });
+  const authorizationToken = authorizationHeader.substring(7);
+  try {
+    const decodedToken = jwt.verify(
+      authorizationToken,
+      process.env.SECRET ?? "localbackendLS(ToChange);"
+    );
+    if (decodedToken === false || decodedToken === undefined) {
+      return res.status(401).send({ message: "Invalid or missing auth token" });
+    }
+    next();
+  } catch (e) {
+    return res.status(401).send({ message: "Invalid or missing auth token" });
   }
 };
