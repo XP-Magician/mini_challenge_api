cded28f25d281178d804854aff65594ff8842530
Diego <diegofabrega18@gmail.com>
2024-04-17 20:54:27 -0400

    UPDATE: SET view engine EJS and main Views directory, DELETE: Unused imports

--- server.mjs
+++ server.mjs
@@ -1,15 +1,19 @@
 // Dependencies
 import Express from "express";
-import CONFIG from "./config/constants.mjs";
+import ViewsRouter from "./routers/ViewsRouter.mjs";
+
+// Constants
 const server = Express();
-
 const PORT = process.env.PORT ?? 54321;
-// Methods
 
-server.get("/", (req, res) => {
-  res.json(CONFIG);
-});
+// Template engine
+server.set("views", "views");
+server.set("view engine", "ejs");
 
+// Main server instance config
+server.use("/", ViewsRouter);
+
+// Server init
 server.listen(PORT, () => {
   console.log(`SERVER LISTENING AT : http://localhost:${PORT}`);
 });
