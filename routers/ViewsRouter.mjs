4bc8b8e327ead10afdc04dc8de73b09f8aec48d8
Diego <diegofabrega18@gmail.com>
2024-04-18 12:36:05 -0400

    UPDATE: add support for showing dinamic URI constant in view

--- routers/ViewsRouter.mjs
+++ routers/ViewsRouter.mjs
@@ -1,10 +1,11 @@
 //Dependencies
 import Express from "express";
+import CONSTANTS from "../config/constants.mjs";
 const router = Express.Router();
 
 // Instructions frontend
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { CONSTANTS });
 });
 
 export default router;
