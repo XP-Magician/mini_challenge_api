7497d2ab6562efbea0f0461972619d5cf5ee4ff9
Diego <diegofabrega18@gmail.com>
2024-04-18 19:08:47 -0400

    UPDATE: ADD endpoints for challenge and dump for onlinepsql, according to TEXT Constants

--- routers/ApiRouter.mjs
+++ routers/ApiRouter.mjs
@@ -1,11 +1,24 @@
 //Dependencies
 import Express from "express";
-import { validateBody, authUser } from "../middlewares/MiddleLogin.mjs";
+import { TEXT } from "../config/constants.mjs";
+import {
+  validateBody,
+  authUser,
+  checkAuth,
+} from "../middlewares/MiddleLogin.mjs";
 const router = Express.Router();
 
 // Instructions API
 router.post("/login", validateBody, authUser, (req, res) => {
   return res.json(req.x_result);
+});
+
+router.get("/challenge", checkAuth, (req, res) => {
+  return res.send(TEXT.CHALLENGE);
+});
+
+router.get("/dumps/onlinepsql", checkAuth, (req, res) => {
+  return res.send(TEXT.POSTGRESCRIPT);
 });
 
 export default router;
