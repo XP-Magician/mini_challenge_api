40c63253c547dc587d2c92d50104007a195dcc72
Diego <diegofabrega18@gmail.com>
2024-04-18 16:23:50 -0400

    UPDATE: Add JWT dependency and middles for: validate body, auth user

--- middlewares/MiddleLogin.mjs
+++ middlewares/MiddleLogin.mjs
@@ -1,12 +1,32 @@
-const validateBody = (req, res, next) => {
+import jwt from "jsonwebtoken";
+import CONFIG from "../config/constants.mjs";
+import { TEXT } from "../config/constants.mjs";
+
+export const validateBody = (req, res, next) => {
   const { username, password } = req.body;
   if (!username || !password) {
-    return res.json({ message: "You forgot sending credentials," }).end();
+    return res.json({ message: "You forgot sending credentials." }).end();
   } else {
     next();
   }
 };
 
-const authUser = (req, res, next) => {
+export const authUser = (req, res, next) => {
   const { username, password } = req.body;
+  if (username === CONFIG.USERNAME && password === CONFIG.PASSWORD) {
+    const token = jwt.sign(
+      { username, password },
+      process.env.SECRET ?? "localbackend"
+    );
+    req.x_result = {
+      access_token: token,
+      instructions: TEXT.INSTRUCTIONS,
+    };
+    next();
+  } else {
+    req.x_result = {
+      error: "Invalid credentials provided, check your entries",
+    };
+    next();
+  }
 };
