756e956a70e18f58ad3b4a1e73ff0decf39e3aac
Diego <diegofabrega18@gmail.com>
2024-04-18 14:14:29 -0400

    ADD: ApiRouter for handle requests

---
+++ routers/ApiRouter.mjs
@@ -0,0 +1,11 @@
+//Dependencies
+import Express from "express";
+import CONSTANTS from "../config/constants.mjs";
+const router = Express.Router();
+
+// Instructions API
+router.post("/login", (req, res) => {
+  // Middlewares and JWT
+});
+
+export default router;
