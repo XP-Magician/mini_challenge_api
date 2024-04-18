85b0165b168911eb1dcfb7c288f835d2b301b82f
Diego <diegofabrega18@gmail.com>
2024-04-18 16:22:55 -0400

    UPDATE: Add TEXT constants for responses purposes

--- config/constants.mjs
+++ config/constants.mjs
@@ -4,4 +4,8 @@ const CONFIG = {
   PASSWORD: "ForisChallenge",
 };
 
+export const TEXT = {
+  INSTRUCTIONS: `instructions :Connect to '/challenge' using your access_token and follow the instructions in the response. Note that the access_token expires in one hour. Include it in the Authorization header in the format 'Bearer {access_token}'. For examples, refer to: https://swagger.io/docs/specification/authentication/bearer-authentication/.`,
+};
+
 export default CONFIG;
