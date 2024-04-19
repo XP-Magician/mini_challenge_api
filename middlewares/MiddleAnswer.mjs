20b515dd6eb04399365e28fd4b85fc95db6d9691
Diego <diegofabrega18@gmail.com>
2024-04-18 20:31:26 -0400

    ADD: Middleware for handling endpoint /validate answers

---
+++ middlewares/MiddleAnswer.mjs
@@ -0,0 +1,28 @@
+import CONSTANTS from "../config/constants.mjs";
+
+export const checkAnswerStructure = (req, res, next) => {
+  const { number_of_groups, answer } = req.body;
+  if (number_of_groups && answer) next();
+  else
+    return res.status(400).json({
+      message: "Missing required fields for answering, check your entries.",
+    });
+};
+
+// Make sure the user is not checking multiple times with differents JWT
+export const rateLimit = (req, res, next) => {};
+
+export const validateAnswer = (req, res, next) => {
+  //Add +1 to rate storage according to IP
+  const { number_of_groups, answer } = req.body;
+  if (number_of_groups !== CONSTANTS.NUMBER_OF_GROUPS) {
+    req.x_message = { message: "incorrect number of groups." };
+    next();
+  }
+  if (answer !== CONSTANTS.CORRECT_ANSWER) {
+    req.x_message = { message: "incorrect answer." };
+    next();
+  }
+  req.x_message = { message: "correct answer!" };
+  next();
+};
